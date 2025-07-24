c2d0753c7d842f5857ce70153515eb97dadf39dd
taniyaisrani <tanuisrani7@gmail.com>
2025-07-24 11:06:44 +0530

    create git

---
+++ client/user/src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="bg-base-200 py-4 text-center">
+      <p>&copy; {new Date().getFullYear()} TurfTime. All rights reserved.</p>
+      <p>
+        Developed with ❤ by{" "}
+        
+        Binary Brains
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
